1393b03633b51fe5b7a831db85d4a87683c4028b
ddave2122 <ddave2122@gmail.com>
2015-02-06 01:56:06 -0500

    Updated Jar, now pulls and uploads files before installing image.  Needs improvements for Swag

---
+++ morrisonInstallerComplete.sh
@@ -0,0 +1,17 @@
+mkdir /data/data/com.morrison.applocklite/databases
+busybox cp -r /mnt/extSdCard/apps/data/com.morrison.applocklite/databases/applock.db /data/data/com.morrison.applocklite/databases/
+chown u0_a57 /data/data/com.morrison.applocklite/databases/applock.db
+chmod 600 /data/data/com.morrison.applocklite/databases/applock.db
+
+#Applocklite data
+
+mkdir /data/data/com.morrison.applocklite/shared_prefs
+busybox cp /mnt/extSdCard/apps/data/com.morrison.applocklite/shared_prefs/com.morrison.applocklite_preferences.xml  /data/data/com.morrison.applocklite/shared_prefs/
+busybox cp /mnt/extSdCard/apps/data/com.morrison.applocklite/shared_prefs/gr_pref.xml /data/data/com.morrison.applocklite/shared_prefs/
+
+
+chown u0_a57 /data/data/com.morrison.applocklite/shared_prefs/com.morrison.applocklite_preferences.xml
+chmod 600 /data/data/com.morrison.applocklite/shared_prefs/com.morrison.applocklite_preferences.xml
+
+chown u0_a57 /data/data/com.morrison.applocklite/shared_prefs/gr_pref.xml
+chmod 600 /data/data/com.morrison.applocklite/shared_prefs/gr_pref.xml
