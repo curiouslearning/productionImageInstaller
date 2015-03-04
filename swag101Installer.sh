4b048746609154d2a9b2db7fce6b24de42cb290e
ddave2122 <ddave2122@gmail.com>
2015-03-04 17:31:44 -0500

    Updated Swag101Installer.sh to uninstall programs before installing them

--- swag101Installer.sh
+++ swag101Installer.sh
@@ -1,5 +1,9 @@
 su
 
+# Remove the old launcher and file mover before installing the new one
+pm uninstall edu.media.mit.prg.launcher
+pm uninstall edu.mit.media.prg.funffilemover
+
 pm set-install-location 2
 
 pm install -s /mnt/external_sd/swagApps/BMA_CO.Phonics_Lv1_Unit10-1.apk
@@ -59,13 +63,18 @@ pm install -s /mnt/external_sd/swagApps/russh.toddler.shapes-1.apk
 pm install -s /mnt/external_sd/swagApps/uk.co.readwritephonics-1.apk
 pm install -s /mnt/external_sd/swagApps/zok.android.shapes-1.apk
 
+# Kids Launcher and funfFileMover
 pm set-install-location 0
-pm install -f /mnt/external_sd/swagApps/funfFileMover.apk
-pm install -f /mnt/external_sd/swagApps/edu.media.mit.prg.launcher-1.apk
+
+# Install the new apps
+pm install /mnt/external_sd/swagApps/funfFileMover.apk
+pm install /mnt/external_sd/swagApps/edu.media.mit.prg.launcher-1.apk
 
 su
 mount -o remount,rw -t ext4 /system
 
+# Remove the old BG Collector before pushing the new one
+rm /system/app/funf_background_collector.apk
 busybox cp /mnt/external_sd/swagApps/systemApps/* /system/app
 
 # Reboot to complete install
