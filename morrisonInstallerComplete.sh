6c93852bc5197e59ec5b5b263ce7d167826f98e5
David Gibbs <ddave2122>
2015-08-03 14:44:07 -0400

    Update to jar to resolve Swag issues

--- morrisonInstallerComplete.sh
+++ morrisonInstallerComplete.sh
@@ -1,7 +1,7 @@
 su
 mkdir /data/data/com.morrison.applocklite/databases
 busybox cp /sdcard/applock/applock.db /data/data/com.morrison.applocklite/databases/
-chown u0_a57 /data/data/com.morrison.applocklite/databases/applock.db
+chown u0_a60 /data/data/com.morrison.applocklite/databases/applock.db
 chmod 600 /data/data/com.morrison.applocklite/databases/applock.db
 
 #Applocklite data
@@ -11,8 +11,8 @@ busybox cp /sdcard/applock/com.morrison.applocklite_preferences.xml  /data/data/
 busybox cp /sdcard/applock/gr_pref.xml /data/data/com.morrison.applocklite/shared_prefs/
 
 
-chown u0_a57 /data/data/com.morrison.applocklite/shared_prefs/com.morrison.applocklite_preferences.xml
+chown u0_a60 /data/data/com.morrison.applocklite/shared_prefs/com.morrison.applocklite_preferences.xml
 chmod 600 /data/data/com.morrison.applocklite/shared_prefs/com.morrison.applocklite_preferences.xml
 
-chown u0_a57 /data/data/com.morrison.applocklite/shared_prefs/gr_pref.xml
+chown u0_a60 /data/data/com.morrison.applocklite/shared_prefs/gr_pref.xml
 chmod 600 /data/data/com.morrison.applocklite/shared_prefs/gr_pref.xml
