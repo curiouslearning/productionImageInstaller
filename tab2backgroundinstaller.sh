13fdd767c2ecd0f80db61e51f128fd8c49c5ea6d
David Gibbs <ddave2122@gmail.com>
2015-09-23 13:27:10 -0400

    now installing funfBackgroundCollector.apk for Tab 2

---
+++ tab2backgroundinstaller.sh
@@ -0,0 +1,12 @@
+su 
+
+mount -o remount,rw /system 
+
+busybox cp /sdcard/funfBackgroundCollector.apk  /system/app/ 
+
+su mount -o remount,rw /system busybox cp /sdcard/funfBackgroundCollector.apk  /system/app/ su 
+
+mount -o remount,rw /system 
+
+busybox cp /sdcard/funfBackgroundCollector.apk  /system/app/ 
+
